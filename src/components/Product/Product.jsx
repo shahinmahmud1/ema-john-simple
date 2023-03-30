f2cad0431c4fa4ecd05d90440cf1175773bbb8ff
Shahin Mahmud <shahin.hbk425@gmail.com>
2023-03-31 05:55:56 +0600

    event handler manage

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props);
     const { img, name, seller, price, ratings } = props.product;
+    console.log(props);
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,7 +15,7 @@ const Product = (props) => {
                 <p className='product-manyfacturer'>Manufacturer : {seller}</p>
                 <p className='product-rating'>Rating : {ratings} star</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
