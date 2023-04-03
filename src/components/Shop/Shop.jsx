4df72f0c9f46a5e813d0562cf5fe2b6cdeb2ac9a
Shahin Mahmud <shahin.hbk425@gmail.com>
2023-04-04 00:41:36 +0600

    updated finally

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -38,7 +38,19 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // console.log('button added', product)
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+        let newCart =[];
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
         setCart(newCart);
         addToDb(product.id);
     }
